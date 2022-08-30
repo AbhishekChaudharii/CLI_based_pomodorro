08f6ffb5325713d28a0a74d75cddfb1f9b99758c
Abhishek <abhishek4080@gmail.com>
2022-08-30 16:30:56 +0530

    Added countdown function--> better way to set a timer

--- pomodorro.py
+++ pomodorro.py
@@ -1,5 +1,5 @@
 from datetime import datetime,timedelta
-#import time 
+import time 
 # import 
 class pomodorro:
     def __init__(self):
@@ -12,7 +12,6 @@ class pomodorro:
     def now_and_now_time(self,flag=0):
         """if flag = 0 it will return datetime raw
         else it will return only time"""
-
         
         now = datetime.now()
         now_time = now.strftime("%H:%M:%S")
@@ -23,46 +22,22 @@ class pomodorro:
             return now_time
     
     def timer(self):
-       
-      now = datetime.now()
-      now_time = now.strftime("%H:%M:%S")
-     
-      return now
+        now = datetime.now()
+        now_time = now.strftime("%H:%M:%S")
+        return now
 
 
-   
+    # define the countdown func.
+    def countdown(self,t):
+        """ Enter in mins"""
+    
+        while t:
+            mins, secs = divmod(t, 60)
+            timer = '{:02d}:{:02d}'.format(mins, secs)
+            print(timer, end="\r")  # print over the same line (end="\r")
+            time.sleep(1) # delay of 1 sec
+            t -= 1
         
-        
-
-    def alert(self):
-         pomo = self.setpomodorro()
-         current_time = self.now_and_now_time()
-         current_time_time = self.now_and_now_time(1)
-         print(f"starting pomodorro at  {current_time_time} ")
-         finish_time = current_time + timedelta(minutes=int(pomo))
-         finish_time_time =  finish_time.strftime("%H:%M:%S")
-         print(f"finishtime is {finish_time_time}")
-
-         while current_time_time != finish_time_time:
-#             
-             delta = datetime.now() - current_time
-#             print(delta.seconds)
-             if delta.seconds >= 1:
-                 current_time = datetime.now()
-                 now_time = current_time.strftime("%H:%M:%S")
-                 print(now_time)
-                 # Update 't' variable to new time
-                 current_time = datetime.now()
-
-
-
-             if finish_time_time == self.now_and_now_time(1):
-           
-                 print("Done!!!!!!!!!!!!!!!!!!!!")
-                 return input("Do you want to start the break? ")
-             
-    def break(self):
-        print("Break will be 30% of the pomodorro time you set")
         
         
         
@@ -86,8 +61,9 @@ if __name__ ==  "__main__":
    
 
     pomodorro1 = pomodorro()
-    pomodorro1.alert()
-    
+    time_min = int(input("Enter time in minutes for your pomodorro session :    "))
+    time_sec = time_min*60
+    pomodorro1.countdown(time_sec)
     
     
     
