17750d7f813a577d5a477b02f22a60218311cadf
Abhishek <abhishek4080@gmail.com>
2022-08-31 16:19:41 +0530

    break feature added __> bug exists!

--- pomodorro.py
+++ pomodorro.py
@@ -27,6 +27,30 @@ class pomodorro:
         return now
 
 
+
+
+    def breakk(self):
+        break_time = int(input("How many minutes of break do you want? "))
+        time_sec = time_min*60
+        t = time_sec
+        while t:
+            mins, secs = divmod(t, 60)
+            timer = '{:02d}:{:02d}'.format(mins, secs)
+            print(timer, end="\r")  # print over the same line (end="\r")
+            time.sleep(1) # delay of 1 sec
+            t -= 1
+
+
+            # ! Code bug in breakk
+            # Start break? yes/no   yes
+#How many minutes of break do you want? 1
+# starts with 2
+# some sec/min  conversion fix needed.
+
+            
+        
+        
+    
     # define the countdown func.
     def countdown(self,t):
         """ Enter in mins"""
@@ -37,6 +61,15 @@ class pomodorro:
             print(timer, end="\r")  # print over the same line (end="\r")
             time.sleep(1) # delay of 1 sec
             t -= 1
+
+        break_ = input("Start break? yes/no   ")
+        break_ = break_.lower()
+        if break_ == "yes":
+           self. breakk()
+
+
+
+        
         
         
         
@@ -62,8 +95,9 @@ if __name__ ==  "__main__":
 
     pomodorro1 = pomodorro()
     time_min = int(input("Enter time in minutes for your pomodorro session :    "))
-    time_sec = time_min*60
-    pomodorro1.countdown(time_sec)
+#    time_sec = time_min*60
+#    pomodorro1.countdown(time_sec)
+    pomodorro1.countdown(time_min)
     
     
     
